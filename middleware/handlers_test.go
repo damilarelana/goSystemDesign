25ce88e290aa0e125d2576a9fa3459670874329d
Damilare Lana <damilarelana@gmail.com>
2020-05-10 23:38:42 +0100

    added TestGetAccountWrongPath()

---
+++ middleware/handlers_test.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+/*
+This asserts for ensuring we get an HTTP 404 when we request an unknown path
+*/
+func TestGetAccountWrongPath(t *testing.T) {
+	Convey("Given a HTTP request for /invalid/123", t, func() {
+		req := httptest.NewRequest("GET", "/invalid/123", nil)
+		resp := httptest.NewRecorder()
+
+		Convey("When the request is handled by the Router", func() {
+			NewRouter().ServeHTTP(resp, req)
+
+			Convey("Then the response should be a 404", func() {
+				So(resp.Code, ShouldEqual, 404)
+			})
+		})
+	})
+}
